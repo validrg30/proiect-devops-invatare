10ec9374cc193dc55eb036fafa5a0101f4142fd3
Valentin Dragomir <valentindragomir30@gmail.com>
2026-02-18 16:02:15 +0200

    Update v5:TEST-bun2

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def home():
     # Citim variabila 'MESAJ'. Daca nu exista, folosim un default.
     mesaj_custom = os.getenv('MESAJ', 'Mesaj Default')
-    retrn f"{mesaj_custom} | Rulez pe: {os.getenv('HOSTNAME')}"
+    return f"{mesaj_custom} | Rulez pe: {os.getenv('HOSTNAME')}"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
