6ed441dce8b114bd4be1b80ebd167cfdca30889f
Valentin Dragomir <valentindragomir30@gmail.com>
2026-02-18 16:29:42 +0200

    first app

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # Citim variabila 'MESAJ'. Daca nu exista, folosim un default.
-    mesaj_custom = os.getenv('MESAJ', 'Mesaj Default')
-    return f"{mesaj_custom} | Rulez pe: {os.getenv('HOSTNAME')}"
+    # Afisam un mesaj si ID-ul containerului (hostname)
+    return f"Salut din Docker! Rulez pe: {os.getenv('HOSTNAME')}"
 
 if __name__ == "__main__":
+    # Rulam serverul pe orice interfata (0.0.0.0) pe portul 5000
     app.run(host='0.0.0.0', port=5000)
