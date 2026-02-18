0e1e1d6dc4e6ceff9a154a1695d98d331013976a
Valentin Dragomir <valentindragomir30@gmail.com>
2026-02-18 17:05:08 +0200

    test

--- app.py
+++ app.py
@@ -6,16 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     # Afisam un mesaj si ID-ul containerului (hostname)
-<<<<<<< HEAD
-    return f"Salut din Docker! Rulez pe: {os.getenv('HOSTNAME')}"
-
-if __name__ == "__main__":
-    # Rulam serverul pe orice interfata (0.0.0.0) pe portul 5000
-    app.run(host='0.0.0.0', port=5000)
-=======
     return f"Salut din Docker noua versiune! Rulez pe: {os.getenv('HOSTNAME')}"
 
 if __name__ == "__main__":
     # Rulam serverul pe orice interfata (0.0.0.0) pe portul 5000
     app.run(host='0.0.0.0', port=5000)
->>>>>>> develop
